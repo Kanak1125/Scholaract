6f4a734fe0d5a50d696fe94959585a25d82a9acd
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-08 13:21:26 +0545

    connected mysql db

--- scholaract/settings.py
+++ scholaract/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'scholaract.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'scholaract', # name of database
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST':'127.0.0.1',
+        # 'PORT': ''
     }
 }
 
