2debfdb29b50416cd45a1e969ba384cb6d41b16e
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-17 17:56:33 +0545

    tried out jazzmin admin panel and fixed sign in button in signup page submitting the form

--- scholaractapp/models.py
+++ scholaractapp/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 # We created our model Users, it is just like creating a table using MySQL queries but the syntax are different
 class Users(models.Model):
+    class Meta:
+        verbose_name = "Users" # label for the model in django admin panel
+        # previously in admin panel, when we created models with a plural name, it appended 's' at the end ogf the model name'jazzmin',
+        verbose_name_plural = "Users" # assinging the verbose_name_plural value as given overcomes this problem
+
     firstName = models.CharField(max_length=300,null=False, blank=False) # cloumns
     lastName = models.CharField(max_length=300,null=False, blank=False)
     email = models.EmailField(max_length=300,null=False, blank=False)
