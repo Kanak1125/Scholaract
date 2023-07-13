385fac7d9471ad705807f2d67a9d309713e88cda
Shrijan Lakhey <lakheyshrijan@gmail.com>
2023-07-13 13:31:46 +0545

    linked nav and footers for the pages

--- scholaractapp/urls.py
+++ scholaractapp/urls.py
@@ -7,18 +7,20 @@ from . import views
 urlpatterns = [
     path('', views.landingPage, name = "landingPage"),
     path('aboutUs/', views.aboutUs, name = "aboutUs"),
-    path('signup/',views.signup, name = "signup"),
-    path('login/',views.login, name = "login"),
-    path('success/',views.success, name = "success"), # temporary url (might be changed later)
-    path('classes/',views.classes, name = "classes"), 
-    path('class/<str:pk>/',views.single_class, name = "class"),     
+    path('faq/', views.faq, name="faq"),
+    # path('support/', views.support, name="support"),
+    path('signup/',views.signup, name="signup"),
+    path('login/',views.login, name="login"),
+    path('success/',views.success, name="success"), # temporary url (might be changed later)
+    path('classes/',views.classes, name="classes"), 
+    path('class/<str:pk>/',views.single_class, name="class"),     
     path('class/<str:pk>/task/', views.task, name="task"),
     # path('class/task/submitTask/<str:pk>', views.submitTask, name="submitTask"),
     path('class/task/deleteTask/<str:pk>', views.deleteTask, name="deleteTask"),
     path('class/<str:pk>/people/', views.people, name="people"),
     path('class/<str:class_pk>/people/removeStudent/<str:student_pk>', views.removeStudent, name="removeStudent"),
     path('class/<str:pk>/report/', views.report, name="report"),
-    path('logout/',views.logout, name = "logout"), 
+    path('logout/',views.logout, name="logout"), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
